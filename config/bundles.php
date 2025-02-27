4d09548ca8a140530ec842cd924beea64a591c4f
Szymon Kostrubiec <github.sk99@gmail.com>
2025-02-27 09:36:36 +0100

    Add dependencies and cofigure ECS and PHPStan

--- config/bundles.php
+++ config/bundles.php
@@ -2,4 +2,6 @@
 
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
+    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
 ];
